01d95998abf9e21547eb1dba6c08038e456bac74
FynnFu <rasidaliev53@gmail.com>
2023-03-05 21:18:22 +0600

    ff

--- StandardQueue/views.py
+++ StandardQueue/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-
+import json
 from StandardQueue.forms import QueueForm
 from StandardQueue.models import QueueModel
 
@@ -11,7 +11,8 @@ def index(request):
         return create_queue(request)
     else:
         host = 'https://' + request.get_host() + f'/join-the-queue/{session_queue}'
-        context = {'host_url': host, 'session': True, 'name': session_queue}
+        users = json.loads(QueueModel.objects.get(name=request.session.get('queue')).ids.replace("'", '"'))
+        context = {'host_url': host, 'session': True, 'name': session_queue, 'users': users['users']}
         return render(request, 'index.html', context)
 
 
@@ -23,7 +24,9 @@ def create_queue(request):
             password = form.cleaned_data.get('password')
             queue = QueueModel.objects.filter(name=name)
             if len(queue) == 0:
-                QueueModel.objects.create(name=name, password=password, ids='[0]')
+                ids = {"users": [{"id": 0, "name": "None"}]}
+                str_ids = json.dumps(ids)
+                QueueModel.objects.create(name=name, password=password, ids=str_ids)
                 request.session['queue'] = name
                 return redirect('index')
             else:
@@ -53,29 +56,42 @@ def delete_queue(request):
 
 def join_the_queue(request, name):
     session_id = request.session.get('id')
-    if session_id is None:
-        queue = QueueModel.objects.get(name=name)
-        ids = eval(queue.ids)
-        last_id = ids[-1]
-        new_id = last_id + 1
-        ids.append(new_id)
-        request.session['id'] = new_id
-        queue.ids = str(ids)
-        queue.save(update_fields=['ids'])
-        session_id = request.session.get('id')
-        context = {'session': True, 'id': session_id, 'name': name}
-        return render(request, 'user_page.html', context)
-    else:
-        context = {'session': True, 'id': session_id, 'name': name}
-        return render(request, 'user_page.html', context)
+    try:
+        if session_id is None:
+            queue = QueueModel.objects.get(name=name)
+            ids_old = queue.ids.replace("'", '"')
+            ids = json.loads(ids_old)
+            your_name = request.POST.get('your_name')
+            ids['users'].append({"id": str(len(ids)), "name": your_name})
+            request.session['id'] = len(ids)
+            queue.ids = json.dumps(ids)
+            queue.save(update_fields=['ids'])
+            session_id = request.session.get('id')
+            context = {'session': True, 'id': session_id, 'name': name}
+            return render(request, 'user_page_leave.html', context)
+        else:
+            context = {'session': True, 'id': session_id, 'name': name}
+            return render(request, 'user_page_leave.html', context)
+    except QueueModel.DoesNotExist:
+        context = {'error': "Срок действия QR-кода истек", 'name': name}
+        return render(request, 'user_page_join.html', context)
 
 
 def leave_the_queue(request, name):
-    queue = QueueModel.objects.get(name=name)
-    ids = eval(queue.ids)
-    ids.remove(request.session['id'])
-    queue.ids = str(ids)
-    queue.save(update_fields=['ids'])
-    del request.session['id']
-    context = {'session': False, 'name': name}
-    return render(request, 'user_page.html', context)
+    try:
+        queue = QueueModel.objects.get(name=name)
+        ids = json.loads(queue.ids.replace("'", '"'))
+        del ids['users'][request.session['id']]
+        queue.ids = str(ids)
+        queue.save(update_fields=['ids'])
+        del request.session['id']
+        context = {'session': False, 'name': name}
+        return render(request, 'user_page_join.html', context)
+    except QueueModel.DoesNotExist:
+        del request.session['id']
+        context = {'error': "Срок действия QR-кода истек", 'name': name}
+        return render(request, 'user_page_leave.html', context)
+    except KeyError:
+        context = {'session': False, 'name': name}
+        return render(request, 'user_page_join.html', context)
+
